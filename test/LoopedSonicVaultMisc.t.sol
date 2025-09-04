2a31b98dc7e3e25651f9b479a37e4bdfe76dd850
Daniel <danielBeets@protonmail.com>
2025-09-04 14:41:11 +0200

    add some assumption checks

--- test/LoopedSonicVaultMisc.t.sol
+++ test/LoopedSonicVaultMisc.t.sol
@@ -12,6 +12,7 @@ import {ILoopedSonicVault} from "../src/interfaces/ILoopedSonicVault.sol";
 import "forge-std/Vm.sol";
 import {IPriceOracle} from "../src/interfaces/IPriceOracle.sol";
 import {AaveCapoRateProvider} from "../src/AaveCapoRateProvider.sol";
+import {IAaveCapoRateProvider} from "../src/interfaces/IAaveCapoRateProvider.sol";
 
 contract LoopedSonicVaultMiscTest is LoopedSonicVaultBase {
     using VaultSnapshot for VaultSnapshot.Data;
@@ -50,24 +51,50 @@ contract LoopedSonicVaultMiscTest is LoopedSonicVaultBase {
 
     function testGrowthInLstRateExceedsMaxRatio() public {
         IPriceOracle priceOracle = vault.AAVE_POOL().ADDRESSES_PROVIDER().getPriceOracle();
+        IAaveCapoRateProvider rateProvider = vault.aaveCapoRateProvider();
         uint256 donateAmount = 10_000_000 ether;
 
-        AaveCapoRateProvider rateProvider = new AaveCapoRateProvider(address(LST), address(PRICE_CAP_ADAPTER));
-
-        uint256 rateBefore = rateProvider.getRate();
-        uint256 lstRateBefore = LST.getRate();
+        uint256 ethPrice = priceOracle.getAssetPrice(address(WETH));
 
         vm.deal(LST_OPERATOR, donateAmount);
 
         vm.prank(LST_OPERATOR);
+        // Donate enough to exceed the CAPO max ratio
         LST.donate{value: donateAmount}();
 
         uint256 rateAfter = rateProvider.getRate();
-        uint256 lstRateAfter = LST.getRate();
+        uint256 lstPriceAfter = priceOracle.getAssetPrice(address(LST));
+        uint256 raterAfterComputed = lstPriceAfter * 1e18 / ethPrice;
 
-        console.log("rateBefore   ", rateBefore);
-        console.log("lstRateBefore", lstRateBefore);
-        console.log("rateAfter    ", rateAfter);
-        console.log("lstRateAfter ", lstRateAfter);
+        VaultSnapshot.Data memory stateAfter = vault.getVaultSnapshot();
+
+        assertGt(LST.getRate(), rateAfter, "LST rate should be greater than the rate provider rate");
+
+        // computed rate is accurate to 7 decimals since aave's base price uses 8 decimals
+        assertApproxEqAbs(rateAfter, raterAfterComputed, 1e11, "Rate should be approximately equal");
+
+        assertEq(
+            stateAfter.lstCollateralAmountInEth,
+            rateProvider.convertToAssets(stateAfter.lstCollateralAmount),
+            "LST collateral amount in ETH should be using the rate provider rate"
+        );
+    }
+
+    function testComputedHealthFactorMatchesAaveValue() public {
+        _setupStandardDeposit();
+
+        VaultSnapshot.Data memory state = vault.getVaultSnapshot();
+
+        (
+            uint256 totalCollateralBase,
+            uint256 totalDebtBase,
+            uint256 availableBorrowsBase,
+            uint256 currentLiquidationThreshold,
+            uint256 ltv,
+            uint256 healthFactor
+        ) = vault.AAVE_POOL().getUserAccountData(address(vault));
+
+        // computed health factor is accurate to 7 decimals since aave's base price uses 8 decimals
+        assertApproxEqAbs(healthFactor, state.healthFactor(), 1e11, "Health factor should be equal");
     }
 }
