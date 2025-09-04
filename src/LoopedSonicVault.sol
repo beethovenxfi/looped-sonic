fadf8e56577147922194cdcf77c7ced69840fd67
Daniel <danielBeets@protonmail.com>
2025-09-04 15:45:13 +0200

    clean up comment

--- src/LoopedSonicVault.sol
+++ src/LoopedSonicVault.sol
@@ -67,8 +67,8 @@ contract LoopedSonicVault is ERC20, AccessControl, ILoopedSonicVault {
     // The stS Aave market uses a correlated asset price oracle (CAPO). To avoid the precision loss of using the base
     // currency (8 decimals), we read the exact rate from the CAP feed and use this rate to price the LST collateral
     // in ETH. This ensures that if the rate growth of stS exceeds the max ratio, the vault will not overprice it's LST
-    // collateral. While unlikely, in the event that the Aave market changes it's oracle, we can update the rate
-    //  provider.
+    // collateral. While unlikely, in the event that the Aave market changes the stS oracle, we allow for the rate
+    // provider to be updated by the admin.
     IAaveCapoRateProvider public aaveCapoRateProvider;
 
     // ---------------------------------------------------------------------
